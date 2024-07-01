5d5a02b6bce07a43b9e4a2f519554a9416f5e137
Antonio Chianese <156539128+antonio-chianese@users.noreply.github.com>
2024-07-01 21:28:11 +0200

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,13 +4,15 @@ end
 
 # ALIASES
 set EDITOR vim
-alias ls='exa --long --header --git -a'
+alias ls='exa --long --git --icons -a'
 alias python='python3'
 alias antonioc='cd /mnt/c/Users/chian/OneDrive/antonioc/'
 alias school='cd /mnt/c/Users/chian/OneDrive/antonioc/school_docs/'
 alias fishrc='$EDITOR ~/.config/fish/config.fish'
 alias sfishrc='source ~/.config/fish/config.fish'
-alias nvimrc='$EDITOR ~/.config/nvim/init.lua'
+alias nvimrc='$EDITOR ~/.vimrc'
+alias vimrc='$EDITOR ~/.vimrc'
+alias alacrittyrc='$EDITOR /mnt/c/Users/chian/AppData/Roaming/alacritty/alacritty.yml'
 alias md='mkdir -p'
 alias rd='rmdir'
 alias tree='tree -a'
